da60021104fcbee133aa1c9203c88fd8b53b04fe
Paula Pniewska <pniewska.paula@gmail.com>
2025-02-20 20:30:42 +0100

    change catalog structure, keep env private, add loop to fetch all sols data

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+// import dotenv from "dotenv";
+
 const latestSol = document.getElementById("latest-sol");
 const latestTemperatureMin = document.getElementById("latest-temperature-min");
 const latestTemperatureMax = document.getElementById("latest-temperature-max");
@@ -8,14 +10,39 @@ const temperatureMin = document.getElementById("temperature-min");
 const temperatureMax = document.getElementById("temperature-max");
 const hws = document.getElementById("hws");
 
+const solList = document.getElementById("sol-list");
+
 async function fetchInSightdata() {
   const response = await fetch(
-    `https://api.nasa.gov/insight_weather/?api_key=&feedtype=json&ver=1.0`
+    `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
   );
   const data = await response.json();
   console.log(data);
 
-  latestSol.innerHTML = `Today Sol: ${data.sol_keys[6]}`;
+  const sols = await { ...data };
+  delete sols.sol_keys;
+  delete sols.validity_checks;
+
+  for (let n in sols) {
+    let div = document.createElement("div");
+    let sol = document.createElement("h2");
+    let mn = document.createElement("p");
+    let mx = document.createElement("p");
+
+    div.className = "sol";
+
+    sol.innerHTML = `Sol: ${n}`;
+    mn.innerHTML = `High: ${sols[n].AT.mn}°F`;
+    mx.innerHTML = `Low: ${sols[n].AT.mx}°F`;
+
+    console.log(n);
+
+    solList.appendChild(div);
+    div.appendChild(sol);
+    div.appendChild(mn);
+    div.appendChild(mx);
+  }
+  latestSol.innerHTML = `Sol ${data.sol_keys[6]}`;
   latestTemperatureMin.innerHTML = `Min temp: ${
     data[Object.getOwnPropertyNames(data)[6]].AT.mn
   }`;
@@ -23,18 +50,6 @@ async function fetchInSightdata() {
     data[Object.getOwnPropertyNames(data)[6]].AT.mx
   }`;
   latestHws.innerHTML = `Horizontal wind speed (av): ${
-    data[Object.getOwnPropertyNames(data)[6]].HWS.av
-  }`;
-
-  // for all week
-  solNumber.innerHTML = `Sol ${Object.getOwnPropertyNames(data)[6]}`;
-  temperatureMin.innerHTML = `Min temp: ${
-    data[Object.getOwnPropertyNames(data)[6]].AT.mn
-  }`;
-  temperatureMax.innerHTML = `Max temp: ${
-    data[Object.getOwnPropertyNames(data)[6]].AT.mx
-  }`;
-  hws.innerHTML = `Horizontal wind speed (av): ${
     data[Object.getOwnPropertyNames(data)[6]].HWS.av
   }`;
 }
